fccba64c521c9b03409a0b01e5904f31b5912fb7
Albert Schreiner <betoceni@outlook.com>
2020-01-04 18:56:53 -0300

    Commit inicial

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import {createStackNavigator} from 'react-navigation';
+
+import Main from './pages/main';
+import Product from './pages/product';
+
+export default createStackNavigator(
+  {
+    Main,
+    Product,
+  },
+  {
+    navigationOptions: {
+      headerTitleStyle: {
+        textAlign: 'center',
+        flexGrow: 1,
+        alignSelf: 'center',
+      },
+      headerStyle: {
+        backgroundColor: '#DA552F',
+      },
+      headerTintColor: '#FFF',
+    },
+  },
+);
